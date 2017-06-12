0423d3c7a19269c41bd0af4ab3bee66768914d53
Debadri Dutta <debadridtt@gmail.com>
2017-06-12 16:59:53 +0530

    Home Automation E-Mail notifications
    This is a full home automation project implemented on this architecture
    house using the Arduino Uno,ESP8266 and different sensor with E-Mail
    notifications using python. You can use the 3g-GSM/GPRS module for SMS
    notifications but this is the cheapest solution.
    The logic and the
    process by which this project has been built doesn't have much
    difference with respect to the real world or the industry.
    All the codes
    are there and have been shared step by step.

---
+++ Home Automation/automation python files/security-automation.py
@@ -0,0 +1,37 @@
+#Debadri Dutta's Code
+
+import time
+import serial
+import smtplib
+import email.utils
+ 
+TO = 'debadridtt@gmail.com'
+GMAIL_USER = 'debadriworkshop@gmail.com'
+GMAIL_PASS = 'Rickstar123'
+ 
+SUBJECT = 'Someone is there at your doorstep'
+TEXT = 'Doorstep Detection-Someone is there at your doorstep. Get the live CCTV footage at: http://192.168.1.2:8080' #Put your IP Webcam IP address
+  
+ser = serial.Serial('COM3', 9600) #PORT NO. OF YOUR ARDUINO, BAUD RATE
+ 
+def send_email():
+    print("Sending Email")
+    smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo
+    smtpserver.login(GMAIL_USER, GMAIL_PASS)
+    header = 'To:' + TO + '\n' + 'From: ' + GMAIL_USER
+    header = header + '\n' + 'Subject:' + SUBJECT + '\n'
+    print(header)
+    msg = header + '\n' + TEXT + ' \n\n'
+    smtpserver.sendmail(GMAIL_USER, TO, msg)
+    smtpserver.close()
+    
+while True:
+    message = ser.readline()
+    print(message)
+    print(message[2]); #TO PRINT THE NUMERICAL VALUE OF "G", INCASE YOU USE SOMETHING ELSE JUST CHANGE THE VALUES HERE AND IN THE NEXT LINE
+    if message[2] == 111: #THE INTEGER VALUE OF "G"
+        send_email()
+    time.sleep(1)
